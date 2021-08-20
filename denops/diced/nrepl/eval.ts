03385630475305abaa53718c26fd5fa4b141cc3a
liquidz <liquidz.uo@gmail.com>
2021-08-21 08:24:57 +0900

    Update evalCode to complete filePath, line and column

--- denops/diced/nrepl/eval.ts
+++ denops/diced/nrepl/eval.ts
@@ -1,4 +1,5 @@
 import { Diced, NreplEvalOption } from "../types.ts";
+import { fns, unknownutil } from "../deps.ts";
 import * as ops from "./operation/core.ts";
 
 export async function evalCode(
@@ -6,7 +7,22 @@ export async function evalCode(
   code: string,
   option?: NreplEvalOption,
 ): Promise<Array<string>> {
-  const res = await ops.evalOp(diced, code, option);
+  const _option = option || {};
+
+  if (_option.filePath == null) {
+    const [filePath, currentPos] = await diced.denops.batch(
+      ["expand", "%p"],
+      ["getcurpos"],
+    );
+    unknownutil.ensureString(filePath);
+    unknownutil.ensureArray<number>(currentPos);
+
+    _option.filePath = filePath;
+    _option.line = currentPos[1];
+    _option.column = currentPos[2];
+  }
+
+  const res = await ops.evalOp(diced, code, _option);
   const values = res.getAll("value");
 
   const verbose = (res.context["verbose"] ?? "true") === "true";
