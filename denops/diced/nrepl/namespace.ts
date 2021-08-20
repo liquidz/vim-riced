dfeee0581c49ad28025f0b0992fd8aa972739548
liquidz <liquidz.uo@gmail.com>
2021-08-21 08:24:20 +0900

    Add inNs to nrepl.namespace

--- denops/diced/nrepl/namespace.ts
+++ denops/diced/nrepl/namespace.ts
@@ -10,3 +10,14 @@ export async function name(diced: Diced): Promise<string> {
   }
   return res[0];
 }
+
+export async function inNs(diced: Diced, nsName?: string): Promise<string> {
+  const ns = nsName ?? await name(diced);
+  const res = await evalCode(diced, `(in-ns '${ns})`, {
+    context: { verbose: "false" },
+  });
+  if (res.length === 0) {
+    return Promise.reject(new Deno.errors.InvalidData("invalid response"));
+  }
+  return res[0];
+}
