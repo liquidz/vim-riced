972252eab6f8acd43d529f4a40a1f9dce31aabba
liquidz <liquidz.uo@gmail.com>
2021-08-21 09:19:59 +0900

    Bump deps

--- denops/diced/deps.ts
+++ denops/diced/deps.ts
@@ -1,13 +1,13 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.5.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.5.0/helper/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v1.5.0/autocmd/mod.ts";
-export * as fns from "https://deno.land/x/denops_std@v1.5.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.5.0/variable/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.7.4/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.7.4/helper/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.7.4/autocmd/mod.ts";
+export * as fns from "https://deno.land/x/denops_std@v1.7.4/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.7.4/variable/mod.ts";
 
 export * as dejs from "https://deno.land/x/dejs@0.10.1/mod.ts";
-export * as fs from "https://deno.land/std@0.104.0/fs/mod.ts";
-export * as nrepl from "https://deno.land/x/deno_nrepl_client@1.0.1/mod.ts";
-export * as path from "https://deno.land/std@0.104.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.105.0/fs/mod.ts";
+export * as nrepl from "https://deno.land/x/deno_nrepl_client@1.0.2/mod.ts";
+export * as path from "https://deno.land/std@0.105.0/path/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
 export * as interceptor from "https://deno.land/x/deno_interceptor@1.0.0/mod.ts";
 
--- denops/diced/test_deps.ts
+++ denops/diced/test_deps.ts
@@ -1,1 +1,1 @@
-export * as asserts from "https://deno.land/std@0.99.0/testing/asserts.ts";
+export * as asserts from "https://deno.land/std@0.105.0/testing/asserts.ts";
