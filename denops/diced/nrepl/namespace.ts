153d47cb1d1b21d7daa6a13786bae67d1d8676af
liquidz <liquidz.uo@gmail.com>
2021-08-25 06:32:25 +0900

    Add namspace.require

--- denops/diced/nrepl/namespace.ts
+++ denops/diced/nrepl/namespace.ts
@@ -1,10 +1,14 @@
+// Namespace functions
+// Functions related to namespace to be processed through nREPL
 import { Diced } from "../types.ts";
-import { evalCode } from "./eval.ts";
+import * as nreplEval from "./eval.ts";
 
 export async function name(diced: Diced): Promise<string> {
-  const res = await evalCode(diced, `(ns-name *ns*)`, {
-    context: { verbose: "false" },
-  });
+  const res = await nreplEval.evalCode(
+    diced,
+    `(clojure.core/ns-name *ns*)`,
+    { context: { verbose: "false" } },
+  );
   if (res.length === 0) {
     return Promise.reject(new Deno.errors.InvalidData("invalid response"));
   }
@@ -13,9 +17,24 @@ export async function name(diced: Diced): Promise<string> {
 
 export async function inNs(diced: Diced, nsName?: string): Promise<string> {
   const ns = nsName ?? await name(diced);
-  const res = await evalCode(diced, `(in-ns '${ns})`, {
-    context: { verbose: "false" },
-  });
+  const res = await nreplEval.evalCode(
+    diced,
+    `(clojure.core/in-ns '${ns})`,
+    { context: { verbose: "false" } },
+  );
+  if (res.length === 0) {
+    return Promise.reject(new Deno.errors.InvalidData("invalid response"));
+  }
+  return res[0];
+}
+
+export async function require(diced: Diced, nsName: string): Promise<string> {
+  const res = await nreplEval.evalCode(
+    diced,
+    `(clojure.core/require '${nsName})`,
+    { context: { verbose: "false" } },
+  );
+
   if (res.length === 0) {
     return Promise.reject(new Deno.errors.InvalidData("invalid response"));
   }
