c06c7fbada003096a764c91abe2c585a4af4774a
liquidz <liquidz.uo@gmail.com>
2021-09-04 07:57:31 +0900

    Bump dependencies

--- denops/diced/deps.ts
+++ denops/diced/deps.ts
@@ -3,16 +3,16 @@ export type {
   Denops,
   Dispatcher,
   Meta,
-} from "https://deno.land/x/denops_std@v1.7.4/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.7.4/helper/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v1.7.4/autocmd/mod.ts";
-export * as fns from "https://deno.land/x/denops_std@v1.7.4/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.7.4/variable/mod.ts";
+} from "https://deno.land/x/denops_std@v1.8.1/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.8.1/helper/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.8.1/autocmd/mod.ts";
+export * as fns from "https://deno.land/x/denops_std@v1.8.1/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.8.1/variable/mod.ts";
 
 export * as dejs from "https://deno.land/x/dejs@0.10.1/mod.ts";
-export * as fs from "https://deno.land/std@0.105.0/fs/mod.ts";
+export * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
 export * as nrepl from "https://deno.land/x/deno_nrepl_client@1.0.3/mod.ts";
-export * as path from "https://deno.land/std@0.105.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
 export * as interceptor from "https://deno.land/x/deno_interceptor@1.0.0/mod.ts";
 
