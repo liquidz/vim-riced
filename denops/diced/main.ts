d0d3e17e5c459d4233e569e3d2b73915f3cc78c8
liquidz <liquidz.uo@gmail.com>
2021-09-12 08:52:44 +0900

    Update to use document_reference plugin

--- denops/diced/main.ts
+++ denops/diced/main.ts
@@ -67,6 +67,7 @@ export async function main(denops: Denops) {
         "complete",
         "clojure_test",
         "diced_debug",
+        "document_reference",
         "info_buffer",
       ]);
 
