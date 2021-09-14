7046e7e4a1bfe808c2f2083230a222e9a1f0450d
liquidz <liquidz.uo@gmail.com>
2021-09-15 08:41:48 +0900

    Bump deno-interceptor

--- denops/diced/deps.ts
+++ denops/diced/deps.ts
@@ -13,7 +13,7 @@ export * as dpsAnon from "https://deno.land/x/denops_std@v1.8.1/anonymous/mod.ts
 export * as snakeCase from "https://deno.land/x/case@v2.1.0/snakeCase.ts";
 export * as dejs from "https://deno.land/x/dejs@0.10.1/mod.ts";
 export * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
-export * as interceptor from "https://deno.land/x/deno_interceptor@1.0.2/mod.ts";
+export * as interceptor from "https://deno.land/x/deno_interceptor@1.0.3/mod.ts";
 export * as nrepl from "https://deno.land/x/deno_nrepl_client@1.0.3/mod.ts";
 export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
