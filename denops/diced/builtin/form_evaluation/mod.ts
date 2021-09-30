802d5121ba882168ba615c030c60316b21bfda52
liquidz <liquidz.uo@gmail.com>
2021-10-01 06:17:49 +0900

    Add DicedEvalAtMark command

--- denops/diced/builtin/form_evaluation/mod.ts
+++ denops/diced/builtin/form_evaluation/mod.ts
@@ -1,9 +1,12 @@
 import { BasePlugin, Command, Diced } from "../../types.ts";
-import { unknownutil } from "../../deps.ts";
+import { dpsFns, unknownutil } from "../../deps.ts";
 
 import * as bufForm from "../../std/buffer/form.ts";
 import * as msg from "../../std/message/core.ts";
 import * as nreplEval from "../../std/nrepl/eval.ts";
+import * as nreplNs from "../../std/nrepl/namespace.ts";
+import * as strParedit from "../../std/string/paredit.ts";
+import * as strNs from "../../std/string/namespace.ts";
 
 async function handleError(diced: Diced, err: unknown): Promise<void> {
   if (err instanceof Deno.errors.NotFound) {
@@ -61,11 +64,62 @@ const EvalBuffer: Command = {
   },
 };
 
+const EvalAtMark: Command = {
+  name: "EvalAtMark",
+  args: "nr2char(getchar())",
+  run: async (diced, args) => {
+    const denops = diced.denops;
+
+    if (args.length !== 1) return;
+    const mark = args[0];
+    if (!unknownutil.isString(mark)) return;
+
+    const pos = await dpsFns.getpos(denops, `'${mark}`);
+    if (pos == [0, 0, 0, 0]) return;
+
+    await dpsFns.bufload(denops, pos[0]);
+    const [src, idx] = await bufForm.getAroundSrcAndIdx(
+      diced,
+      { line: pos[1], column: pos[2] },
+      1,
+      pos[0],
+    );
+
+    const range = strParedit.parentFormRange(src, idx);
+    if (range == null || range === [-1, -1]) {
+      await msg.warning(diced, "NotFound");
+      return;
+    }
+    const code = src.substring(range[0], range[1]);
+
+    const [srcHead] = await bufForm.getAroundSrcAndIdx(
+      diced,
+      { line: 1, column: 1 },
+      50,
+      pos[0],
+    );
+
+    const nsIdx = srcHead.search(/\((ns|in-ns)[ \r\n]/);
+    const nsRange = strParedit.rangeForDefun(srcHead, nsIdx);
+    const nsForm = srcHead.substring(nsRange[0], nsRange[1]);
+    const nsName = strNs.extractName(nsForm);
+
+    try {
+      await nreplNs.require(diced, nsName);
+      await nreplEval.evalCode(diced, code, { namespace: nsName });
+    } catch (err) {
+      await handleError(diced, err);
+    }
+    return;
+  },
+};
+
 export class Plugin extends BasePlugin {
   readonly commands = [
     EvalCode,
     EvalOuterList,
     EvalOuterTopList,
     EvalBuffer,
+    EvalAtMark,
   ];
 }
