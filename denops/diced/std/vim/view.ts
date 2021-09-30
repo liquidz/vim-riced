27b597b6be5ddbf13303577dca7e8aa0f895ae57
liquidz <liquidz.uo@gmail.com>
2021-10-01 06:11:21 +0900

    Update vim.view/saveView and restView

--- denops/diced/std/vim/view.ts
+++ denops/diced/std/vim/view.ts
@@ -1,26 +1,23 @@
-import { dpsFns, dpsVars } from "../../deps.ts";
+import { dpsHelper } from "../../deps.ts";
 import { Diced } from "../../types.ts";
+import * as stdFn from "../fn/mod.ts";
 
-type SavedView = {
-  reg: unknown;
-  bufnr: unknown;
-  view: unknown;
-  marks: unknown;
-};
+const viewInitialize = stdFn.memoize(async (diced: Diced) => {
+  const path = new URL(".", import.meta.url);
+  path.pathname = path.pathname + "view.vim";
+  await dpsHelper.load(diced.denops, path);
+}, (_) => "once");
 
-export async function saveView(diced: Diced): Promise<SavedView> {
-  const denops = diced.denops;
-  return {
-    reg: await dpsVars.register.get(denops, "@"),
-    bufnr: await dpsFns.bufnr(denops, "%"),
-    view: await dpsFns.winsaveview(denops),
-    marks: "FIXME",
-  };
+export async function saveView(diced: Diced): Promise<unknown> {
+  await viewInitialize(diced);
+  return await diced.denops.call("DicedSaveView");
 }
 
-export async function restView(diced: Diced, view: SavedView): Promise<void> {
-  const denops = diced.denops;
-  await denops.cmd(`b ${view.bufnr}`);
-  await dpsFns.winrestview(denops, view.view);
-  await dpsVars.register.set(denops, "@", view.reg);
+export async function restView(
+  diced: Diced,
+  savedView: unknown,
+): Promise<void> {
+  await viewInitialize(diced);
+  await diced.denops.call("DicedRestView", savedView);
+  return;
 }
