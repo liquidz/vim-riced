b06aa2a54de906ca36b451e7bddeaeed565da968
liquidz <liquidz.uo@gmail.com>
2023-01-02 22:57:41 +0900

    deps: Add denops_std/function as dependency

--- denops/icedon/deps.ts
+++ denops/icedon/deps.ts
@@ -4,8 +4,9 @@ export * as batch from "https://deno.land/x/denops_std@v3.10.0/batch/mod.ts";
 export * as helper from "https://deno.land/x/denops_std@v3.10.0/helper/mod.ts";
 export * as unknownutil from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
 export * as variable from "https://deno.land/x/denops_std@v3.10.0/variable/mod.ts";
+export * as vimFn from "https://deno.land/x/denops_std@v3.10.0/function/mod.ts";
 
-export * as path from "https://deno.land/std@0.108.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
 export * as async from "https://deno.land/std@0.170.0/async/mod.ts";
 
 export * as kase from "https://deno.land/x/case@v2.1.0/mod.ts";
