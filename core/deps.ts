34924539f4aeb03b92bf0c47427372eb793445ca
liquidz <liquidz.uo@gmail.com>
2023-01-02 15:14:43 +0900

    chore: Remove old files

--- core/deps.ts
+++
@@ -1,3 +0,0 @@
-export * as interceptor from "https://deno.land/x/deno_interceptor@2.0.2/mod.ts";
-export * as nrepl from "https://deno.land/x/deno_nrepl_client@1.0.5/mod.ts";
-export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.2/mod.ts";
--- core/mod.ts
+++
@@ -1,6 +0,0 @@
-export * from "./connection.ts";
-export * from "./connection_manager.ts";
-export * from "./diced.ts";
-export * from "./interceptor.ts";
-export * from "./request.ts";
-export * from "./types.ts";
