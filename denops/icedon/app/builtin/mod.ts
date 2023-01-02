c9acc683797bbb7d31d1e1013b76f34bf5f4ce48
liquidz <liquidz.uo@gmail.com>
2023-01-02 23:04:16 +0900

    feat: Add InfoBuffer API

--- denops/icedon/app/builtin/mod.ts
+++ denops/icedon/app/builtin/mod.ts
@@ -2,11 +2,14 @@ import { ApiPlugin, InterceptorPlugin } from "../../types.ts";
 
 import * as apiConnection from "./connection/mod.ts";
 import * as apiEvaluation from "./evaluation/mod.ts";
+import * as apiInfoBuffer from "./info_buffer/mod.ts";
+
 import * as interceptorNreplOutput from "./nrepl_output/mod.ts";
 
 export const apiPlugins: ApiPlugin[] = [
   new apiConnection.Api(),
   new apiEvaluation.Api(),
+  new apiInfoBuffer.Api(),
 ];
 export const interceptorPlugins: InterceptorPlugin[] = [
   new interceptorNreplOutput.Interceptor(),
