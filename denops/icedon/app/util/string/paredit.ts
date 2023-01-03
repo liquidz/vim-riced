b712522a1dabf04df8232dc907f8b24026b1824e
liquidz <liquidz.uo@gmail.com>
2023-01-03 11:30:50 +0900

    feat: Add app.util.string.paredit

---
+++ denops/icedon/app/util/string/paredit.ts
@@ -0,0 +1,21 @@
+import { paredit } from "../../../deps.ts";
+
+export function forwardSexp(src: string, idx: number): number {
+  const ast = paredit.parse(src);
+  return paredit.navigator.forwardSexp(ast, idx) + 1;
+}
+
+export function backwardSexp(src: string, idx: number): number {
+  const ast = paredit.parse(src);
+  return paredit.navigator.backwardSexp(ast, idx) + 1;
+}
+
+export function sexpRange(src: string, idx: number): [number, number] {
+  const ast = paredit.parse(src);
+  return paredit.navigator.sexpRange(ast, idx);
+}
+
+export function rangeForDefun(src: string, idx: number): [number, number] {
+  const ast = paredit.parse(src);
+  return paredit.navigator.rangeForDefun(ast, idx);
+}
