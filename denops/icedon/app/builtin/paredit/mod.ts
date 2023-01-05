77f95b16a7b59d328de7c502e74d5a8af71862e5
liquidz <liquidz.uo@gmail.com>
2023-01-05 16:11:17 +0900

    fix: Fix getCurrentTopForm

--- denops/icedon/app/builtin/paredit/mod.ts
+++ denops/icedon/app/builtin/paredit/mod.ts
@@ -1,6 +1,12 @@
-import { ApiPlugin, App } from "../../../types.ts";
-import { unknownutil, vimFn } from "../../../deps.ts";
-import { rangeForDefun } from "../../util/string/paredit.ts";
+import { ApiPlugin, App, Position } from "../../../types.ts";
+import { helper, unknownutil, vimFn } from "../../../deps.ts";
+import { memoize } from "../../util/fn/memoize.ts";
+
+const rangeInitialize = memoize(async (app: App) => {
+  const path = new URL(".", import.meta.url);
+  path.pathname += "range.vim";
+  await helper.load(app.denops, path);
+}, (_) => "once");
 
 /**
  * Returns code and the starting line number
@@ -8,17 +14,28 @@ import { rangeForDefun } from "../../util/string/paredit.ts";
 const getCurrentTopForm = {
   name: "icedon_get_current_top_form",
   run: async (app: App, _args: unknown[]) => {
-    const denops = app.denops;
-    const startPos = await vimFn.searchpos(denops, "^\\S", "bcnW");
-    unknownutil.assertArray<number>(startPos);
+    await rangeInitialize(app);
 
-    const lines = await vimFn.getline(denops, startPos[0], "$");
+    const range = await app.denops.call("IcedonGetTopFormRange");
+    unknownutil.assertArray<number>(range);
+
+    if (range[0] === 0) {
+      return Deno.errors.NotFound;
+    }
+
+    // NOTE: start and end is 1-based index
+    const start: Position = [range[0], range[1]];
+    const end: Position = [range[2], range[3]];
+    const denops = app.denops;
+
+    const lines = await vimFn.getline(denops, start[0], end[0]);
     unknownutil.assertArray<string>(lines);
 
-    const code = lines.join("\n");
-    const [_, endIndex] = rangeForDefun(code, 0);
+    const len = lines.length;
+    lines[0] = lines[0].substring(start[1] - 1);
+    lines[len - 1] = lines[len - 1].substring(0, end[1]);
 
-    return [code.substring(0, endIndex), startPos[0]];
+    return [lines.join("\n"), start[0], start[1]];
   },
 };
 
