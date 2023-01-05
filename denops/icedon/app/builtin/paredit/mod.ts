ef5d77100a4b4812663d23b39915982c02e64fab
liquidz <liquidz.uo@gmail.com>
2023-01-05 16:43:45 +0900

    feat: Add icedon_get_ns_form API

--- denops/icedon/app/builtin/paredit/mod.ts
+++ denops/icedon/app/builtin/paredit/mod.ts
@@ -8,6 +8,32 @@ const rangeInitialize = memoize(async (app: App) => {
   await helper.load(app.denops, path);
 }, (_) => "once");
 
+function unknownToRange(range: unknown): [Position, Position] {
+  unknownutil.assertArray<number>(range);
+  if (range[0] === 0) {
+    throw Deno.errors.NotFound;
+  }
+
+  // NOTE: start and end is 1-based index
+  const start: Position = [range[0], range[1]];
+  const end: Position = [range[2], range[3]];
+  return [start, end];
+}
+
+async function getLinesByRange(
+  app: App,
+  start: Position,
+  end: Position,
+): Promise<string[]> {
+  const lines = await vimFn.getline(app.denops, start[0], end[0]);
+  unknownutil.assertArray<string>(lines);
+
+  const len = lines.length;
+  lines[0] = lines[0].substring(start[1] - 1);
+  lines[len - 1] = lines[len - 1].substring(0, end[1]);
+  return lines;
+}
+
 /**
  * Returns code and the starting line number
  */
@@ -15,31 +41,27 @@ const getCurrentTopForm = {
   name: "icedon_get_current_top_form",
   run: async (app: App, _args: unknown[]) => {
     await rangeInitialize(app);
-
     const range = await app.denops.call("IcedonGetTopFormRange");
-    unknownutil.assertArray<number>(range);
-
-    if (range[0] === 0) {
-      return Deno.errors.NotFound;
-    }
-
     // NOTE: start and end is 1-based index
-    const start: Position = [range[0], range[1]];
-    const end: Position = [range[2], range[3]];
-    const denops = app.denops;
+    const [start, end] = unknownToRange(range);
+    const lines = await getLinesByRange(app, start, end);
+    return [lines.join("\n"), start[0], start[1]];
+  },
+};
 
-    const lines = await vimFn.getline(denops, start[0], end[0]);
-    unknownutil.assertArray<string>(lines);
-
-    const len = lines.length;
-    lines[0] = lines[0].substring(start[1] - 1);
-    lines[len - 1] = lines[len - 1].substring(0, end[1]);
-
+const getNsForm = {
+  name: "icedon_get_ns_form",
+  run: async (app: App, _args: unknown[]) => {
+    await rangeInitialize(app);
+    const range = await app.denops.call("IcedonGetNsFormRange");
+    // NOTE: start and end is 1-based index
+    const [start, end] = unknownToRange(range);
+    const lines = await getLinesByRange(app, start, end);
     return [lines.join("\n"), start[0], start[1]];
   },
 };
 
 export class Api extends ApiPlugin {
   readonly name = "icedon builtin paredit";
-  readonly apis = [getCurrentTopForm];
+  readonly apis = [getCurrentTopForm, getNsForm];
 }
--- denops/icedon/app/builtin/paredit/range.vim
+++ denops/icedon/app/builtin/paredit/range.vim
@@ -24,14 +24,35 @@ function! IcedonGetCurrentFormRange() abort
   try
     let start = searchpos('(', "bcW", 0, 0, s:default_skip)
     if start == [0, 0]
-      return [[0, 0], [0, 0]]
+      return [0, 0, 0, 0]
     endif
 
     let end = searchpairpos('(', '', ')', 'nW', s:default_skip)
     if end == [0, 0]
-      return [[0, 0], [0, 0]]
+      return [0, 0, 0, 0]
     endif
-    return [start, end]
+    return [start[0], start[1], end[0], end[1]]
+  finally
+    call winrestview(view)
+  endtry
+endfunction
+
+function! IcedonGetNsFormRange() abort
+  let view = winsaveview()
+  try
+    let start = searchpos('(ns[ \\r\\n ]', "bcW", 0, 0, s:default_skip)
+    if start == [0, 0]
+      let start = searchpos('(in-ns[ \\r\\n ]', "bcW", 0, 0, s:default_skip)
+    endif
+    if start == [0, 0]
+      return [0, 0, 0, 0]
+    endif
+
+    let end = searchpairpos('(', '', ')', 'nW', s:default_skip)
+    if end == [0, 0]
+      return [0, 0, 0, 0]
+    endif
+    return [start[0], start[1], end[0], end[1]]
   finally
     call winrestview(view)
   endtry
