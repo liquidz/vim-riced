3f89041dbd214cc24afe4ed45e592db75b2bf21c
liquidz <liquidz.uo@gmail.com>
2023-01-08 04:36:31 +0900

    feat: Add positionToIndex to util.string.paredit

--- denops/@icedon-plugin/util/string/paredit.ts
+++ denops/@icedon-plugin/util/string/paredit.ts
@@ -1,10 +1,19 @@
 import { paredit } from "../../deps.ts";
+import { Position } from "../../types.ts";
 
 type AST = Record<string, unknown>;
 type Source = string | AST;
 
 export function parse(src: string): AST {
   return paredit.parse(src);
+}
+
+/**
+ * Position should be 0-based index
+ */
+export function positionToIndex(s: string, pos: Position): number {
+  return s.split(/\n/).slice(0, pos[0]).join("\n").length +
+    pos[1] + (pos[0] > 0 ? 1 : 0);
 }
 
 export function forwardSexp(src: Source, idx: number): number {
--- denops/@icedon-plugin/util/string/paredit_test.ts
+++ denops/@icedon-plugin/util/string/paredit_test.ts
@@ -1,6 +1,15 @@
 import { asserts } from "../../test_deps.ts";
 import * as sut from "./paredit.ts";
 
+Deno.test("positionToIndex", () => {
+  asserts.assertEquals(sut.positionToIndex("foo\nbar\nbaz", [0, 0]), 0);
+  asserts.assertEquals(sut.positionToIndex("foo\nbar\nbaz", [0, 1]), 1);
+  asserts.assertEquals(sut.positionToIndex("foo\nbar\nbaz", [1, 0]), 4);
+  asserts.assertEquals(sut.positionToIndex("foo\nbar\nbaz", [1, 1]), 5);
+  asserts.assertEquals(sut.positionToIndex("foo\nbar\nbaz", [2, 0]), 8);
+  asserts.assertEquals(sut.positionToIndex("foo\nbar\nbaz", [2, 1]), 9);
+});
+
 Deno.test("forwardSexp", () => {
   asserts.assertEquals(sut.forwardSexp("foo bar", 0), 4);
   asserts.assertEquals(sut.forwardSexp(sut.parse("foo bar"), 0), 4);
