d5706b4f401eca609a8219061326edada73d9f6d
liquidz <liquidz.uo@gmail.com>
2023-01-09 20:03:14 +0900

    deps: Add zod as dependency

--- denops/@icedon-plugin/deps.ts
+++ denops/@icedon-plugin/deps.ts
@@ -5,3 +5,4 @@ export * as unknownutil from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
 export * as path from "https://deno.land/std@0.171.0/path/mod.ts";
 export { default as paredit } from "npm:paredit.js@0.2.13";
 export * as vimFn from "https://deno.land/x/denops_std@v3.12.1/function/mod.ts";
+export { z } from "https://deno.land/x/zod@v3.20.2/mod.ts";
