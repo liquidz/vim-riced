51740152a03f3256a3b200f2fa30a4e16fc90554
liquidz <liquidz.uo@gmail.com>
2023-07-27 06:35:26 +0900

    fix: Update connected interceptor to create namespace

--- denops/@riced_interceptors/connected.ts
+++ denops/@riced_interceptors/connected.ts
@@ -3,8 +3,12 @@ import { InterceptorContext, InterceptorExecutionError, std } from "./deps.ts";
 type Arg = std.connect.Arg;
 
 export class Interceptor extends std.connect.BaseConnectInterceptor {
+  readonly requireOthers = true;
+
   async leave(ctx: InterceptorContext<Arg>): Promise<InterceptorContext<Arg>> {
     if (ctx.arg.params.isConnected === true) {
+      await std.namespace.create(ctx.arg.app);
+
       await std.message.info(
         ctx.arg.app,
         "connected",
